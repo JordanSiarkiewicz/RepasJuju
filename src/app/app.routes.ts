a39315f9a0082b3e564916b69d6a904e82c15948
Jordan S <jordan.siarkiewicz@gmail.com>
2024-10-23 15:19:35 +0200

    suppression

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { RepasComponent } from './pages/repas/repas.component';
-
-export const routes: Routes = [
-    {path: 'repas', component: RepasComponent},
-];
