ad204134d6abbef8bafb43b56767a85b665b7701
Jordan S <jordan.siarkiewicz@gmail.com>
2024-10-23 15:59:37 +0200

    modif git page6

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { RepasComponent } from './pages/repas/repas.component';
+
+export const routes: Routes = [
+    {path: 'repas', component: RepasComponent},
+];
